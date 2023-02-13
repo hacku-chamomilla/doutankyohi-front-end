c839fe3adfdfa1d25a0cfe8f721b4568c7a96fb4
ltoppyl <toppyajibutu@gmail.com>
2023-02-13 14:02:41 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { TempComponent } from "../src/components/TempComponent";
+
+const Home: NextPage = () => {
+  return <TempComponent />;
+};
+
+export default Home;
---
+++ src/components/TempComponent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TempComponent = () => {
+  return <p>Hello Next.js!</p>;
+};
