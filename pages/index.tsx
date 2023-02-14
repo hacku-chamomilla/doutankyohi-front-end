0f41b8232c0a8059acd9398a7cc52c8611bec190
ltoppyl <toppyajibutu@gmail.com>
2023-02-14 21:45:46 +0900

    refactor: コンポーネントを可視化するための部分を削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
 import React from "react";
 
-import { TempComponent } from "@/components/TempComponent";
-
 const Home: NextPage = () => {
-  return <TempComponent />;
+  return <p>Hello Next.Js</p>;
 };
 
 export default Home;
