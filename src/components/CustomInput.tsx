c224840f26514dbde0a6a7151ea42c3193d0f81f
KEiJey <keijey1015@outlook.jp>
2023-02-15 19:10:58 +0900

    feat: 入力された文字列を保存して下部に表示出来るようにしました。
    下部に表示出来るようにしているのは確認の為です。

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Input, Text } from "@chakra-ui/react";
+import { Button, ButtonGroup, Input, Text } from "@chakra-ui/react";
 
 import { HSpacer, VSpacer } from "@/components/common/Spacer";
 
@@ -10,11 +10,19 @@ type Props = {
 };
 
 export const CustomInput = ({ title, placeholder }: Props) => {
+  const [text, setText] = useState("");
+
   return (
     <>
       <Text fontSize="xl">{title}</Text>
       <VSpacer size={8} />
-      <Input placeholder={placeholder} size="lg" />
+      <Input
+        value={text}
+        placeholder={placeholder}
+        size="lg"
+        onChange={(event) => setText(event.target.value)}
+      />
+      <p>{text}</p>
     </>
   );
 };
