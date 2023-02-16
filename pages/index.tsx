d74ac440401b9ee4b8d9bb15aec7e2104fcd481f
ltoppyl <toppyajibutu@gmail.com>
2023-02-16 13:09:55 +0900

    feat: / から /start-game への遷移の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import React from "react";
 
+import { Button } from "@chakra-ui/react";
+
 const Home: NextPage = () => {
-  return <p>Hello Next.Js</p>;
+  const router = useRouter();
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          router.push("/start-game");
+        }}
+      >
+        To Start Game Page
+      </Button>
+    </>
+  );
 };
 
 export default Home;
