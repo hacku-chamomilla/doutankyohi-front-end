e9447c73184499b5f170ff26d4c2ba0dfeba3ada
zo-ma <zero.one.mean.ability@outlook.jp>
2023-02-20 15:16:21 +0900

    fix:位置調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import { useRouter } from "next/router";
 import React from "react";
 
 import {
+  Box,
   Button,
+  Center,
   Icon,
   Modal,
   ModalBody,
@@ -15,58 +17,62 @@ import {
   ModalOverlay,
   Text,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
+
+import { VSpacer } from "@/components/common/Spacer";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Icon
-        pos="absolute"
-        top="4"
-        right="7"
-        as={QuestionOutlineIcon}
-        boxSize={6}
-        onClick={onOpen}
-        _hover={{ cursor: "pointer" }}
-      />
+      <VSpacer size={4} />
+      <Box display="flex" justifyContent="flex-end">
+        <VStack>
+          <Icon
+            as={QuestionOutlineIcon}
+            boxSize={6}
+            onClick={onOpen}
+            _hover={{ cursor: "pointer" }}
+          />
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>ゲームのルール</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>ゲームのルールの詳細をここに入れる</ModalBody>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>ゲームのルール</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>ゲームのルールの詳細をここに入れる</ModalBody>
+              <ModalFooter>
+                <Button colorScheme="blue" onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+          <Text fontSize="xs">How to play</Text>
+        </VStack>
+      </Box>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      <Text fontSize="xs" pos="absolute" top="10" right="3">
-        How to play
-      </Text>
+      <VSpacer size={40} />
 
-      <Text fontSize="3xl" pos="absolute" top="200" left="100">
-        タイトルロゴ
-      </Text>
-      {/* ここはロゴ作って差し替えたい */}
-      <Button
-        pos="absolute"
-        top="450"
-        left="61"
-        h={"60px"}
-        w={"270px"}
-        colorScheme="blue"
-        onClick={() => {
-          router.push("/start-game");
-        }}
-      >
-        START
-      </Button>
+      <Center>
+        <VStack>
+          {/* TODO: ここはロゴ作って差し替えたい */}
+          <Text fontSize="3xl">タイトルロゴ</Text>
+          <VSpacer size={36} />
+          <Button
+            h={"60px"}
+            w={"270px"}
+            colorScheme="blue"
+            onClick={() => {
+              router.push("/start-game");
+            }}
+          >
+            START
+          </Button>
+        </VStack>
+      </Center>
     </>
   );
 };
