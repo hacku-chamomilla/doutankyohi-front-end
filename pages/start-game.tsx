6ff0e8fb21a0e5d15aff03e971649f7c82840123
ltoppyl <toppyajibutu@gmail.com>
2023-02-20 17:55:56 +0900

    Merge pull request

--- pages/start-game.tsx
+++ pages/start-game.tsx
@@ -1,19 +1,50 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 
-import { Button } from "@chakra-ui/react";
+import { Button, IconButton } from "@chakra-ui/react";
+
+import { HSpacer, VSpacer } from "@/components/common/Spacer";
 
 const StartGame: NextPage = () => {
   const router = useRouter();
   return (
     <>
-      <Button
+      <HSpacer size={2} />
+      <IconButton
+        colorScheme="gray"
+        icon={<ArrowBackIcon />}
+        isRound={true}
+        aria-label={"homeBack"}
         onClick={() => {
           router.push("/");
         }}
+      />
+      <VSpacer size={40} />
+      <HSpacer size={12} />
+      <Button
+        w="30%"
+        colorScheme="blue"
+        color={"white"}
+        onClick={() => {
+          router.push("/create-room");
+        }}
       >
-        To Home
+        ルーム作成
+      </Button>
+
+      <VSpacer size={12} />
+      <HSpacer size={12} />
+      <Button
+        w="30%"
+        colorScheme="blue"
+        color={"white"}
+        onClick={() => {
+          router.push("/join-room");
+        }}
+      >
+        ルームに参加
       </Button>
     </>
   );
