2e54c8ad707d71135b8a43df974014a7fa2c553b
KEiJey <keijey1015@outlook.jp>
2023-02-21 14:48:38 +0900

    feat: ルーム作成のためのUIの作成

--- pages/create-room.tsx
+++ pages/create-room.tsx
@@ -1,8 +1,56 @@
 import type { NextPage } from "next";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+import { Button, Center, Text, VStack } from "@chakra-ui/react";
+
+import { VSpacer } from "@/components/common/Spacer";
+import { avatarList } from "@/components/data/AvatarList";
+import { NNAndIcon } from "@/components/NNAndIcon";
+import { PageBackIcon } from "@/components/PageBackIcon";
 
 const CreateRoom: NextPage = () => {
-  return <p>Create Room</p>;
+  const router = useRouter();
+  const [nickname, setNickname] = useState("");
+  const [avatarIndex, setAvatarIndex] = useState(0);
+  return (
+    <>
+      <VSpacer size={4} />
+      <PageBackIcon pass={"/start-game"} />
+      <VSpacer size={12} />
+
+      <Center>
+        <VStack>
+          <Text fontSize={40}>ルームの作成</Text>
+          <VSpacer size={8} />
+          <Text fontSize={20}>「ルームの作成」を押すと,</Text>
+          <Text fontSize={20}>ルームIDが自動で生成されます</Text>
+          <VSpacer size={8} />
+          <NNAndIcon
+            title={"ニックネーム"}
+            subtitle={"アイコン選択"}
+            nickname={nickname}
+            placeholder={"ふかまる"}
+            avatarList={avatarList}
+            avatarIndex={avatarIndex}
+            setNickname={setNickname}
+            setAvatarIndex={setAvatarIndex}
+          />
+          <VSpacer size={24} />
+          <Button
+            colorScheme="blue"
+            color={"white"}
+            minW={64}
+            onClick={() => {
+              router.push("/game");
+            }}
+          >
+            ルーム作成
+          </Button>
+        </VStack>
+      </Center>
+    </>
+  );
 };
 
 export default CreateRoom;
