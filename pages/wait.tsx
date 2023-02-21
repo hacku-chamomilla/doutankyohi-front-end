0575f0811e776b25fd6e7ce0292b72e7554599ea
ltoppyl <toppyajibutu@gmail.com>
2023-02-21 22:36:25 +0900

    feat: 自分の playerId を確認

--- pages/wait.tsx
+++ pages/wait.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
 import React from "react";
+import { useRecoilValue } from "recoil";
+
+import { playerId } from "@/store/Recoil";
 
 const Wait: NextPage = () => {
-  return <p>/wait</p>;
+  const _ = useRecoilValue(playerId);
+  return <p>/wait {_.id}</p>;
 };
 
 export default Wait;
