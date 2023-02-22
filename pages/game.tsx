bdab545bf0800a629f91996753a979a62776dab3
ltoppyl <toppyajibutu@gmail.com>
2023-02-22 18:27:07 +0900

    feat: step が1の時に限った role ごとの画面の振り分け

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,8 +1,44 @@
+import axios from "axios";
 import type { NextPage } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+
+import { HowToDecideTheme } from "@/components/game/HowToDecideTheme";
+import { ThinkingTheme } from "@/components/game/ThinkingTheme";
+import { Wait } from "@/components/game/Wait";
+
+import { RecoilPlayer } from "@/store/Recoil";
+
+import { BASE_URL } from "../src/data/BaseUrl";
 
 const Game: NextPage = () => {
-  return <p>/game</p>;
+  const player = useRecoilValue(RecoilPlayer);
+  const [role, setRole] = useState<number>();
+
+  // eslint-disable-next-line no-console
+  console.log(`----------\nplayerId: ${player.id}\n----------`); // TODO 作業用のログ, いつかは消す
+
+  useEffect(() => {
+    const url = BASE_URL + "get-role";
+    axios
+      .get(url, {
+        params: { playerId: player.id },
+      })
+      .then((res) => {
+        setRole(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      {role == 1 && <Wait />}
+      {role == 2 && <HowToDecideTheme />}
+      {role == 3 && <ThinkingTheme />}
+    </>
+  );
 };
 
 export default Game;
