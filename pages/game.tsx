cef44734146f5abcb1e87c3d135b121f5ed310fc
ltoppyl <toppyajibutu@gmail.com>
2023-02-22 21:07:21 +0900

    feat: 回答者が step6 になった時に回答画面へ遷移

--- pages/game.tsx
+++ pages/game.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
+import { Answer } from "@/components/game/Answer";
 import { HowToDecideTheme } from "@/components/game/HowToDecideTheme";
 import { ThinkingTheme } from "@/components/game/ThinkingTheme";
 import { Wait } from "@/components/game/Wait";
@@ -18,7 +19,13 @@ const Game: NextPage = () => {
   const router = useRouter();
   const player = useRecoilValue(RecoilPlayer);
   const [role, setRole] = useState<number>();
+  const [step, setStep] = useState<number>(1); // /game に最初に到達する時点でstep=1が保証されている(はず)
 
+  const exampleAnswerHintList = [
+    { text: "ジャッカル", isDelete: true },
+    { text: "ジャッカル", isDelete: true },
+    { text: "かいぎしつ", isDelete: false },
+  ];
   // eslint-disable-next-line no-console
   console.log(`----------\nplayerId: ${player.id}\n----------`); // TODO 作業用のログ, いつかは消す
 
@@ -38,7 +45,8 @@ const Game: NextPage = () => {
 
   return (
     <>
-      {role == 1 && <Wait />}
+      {role == 1 && step == 1 && <Wait setStep={setStep} />}
+      {role == 1 && step == 6 && <Answer hintList={exampleAnswerHintList} />}
       {role == 2 && <HowToDecideTheme />}
       {role == 3 && <ThinkingTheme />}
     </>
