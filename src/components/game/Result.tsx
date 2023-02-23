9db138cee5b34ac0c995fd605df5ea02aa59dd0f
ltoppyl <toppyajibutu@gmail.com>
2023-02-23 15:49:17 +0900

    feat: Result から次に進む導線の追加

--- src/components/game/Result.tsx
+++ src/components/game/Result.tsx
@@ -1,19 +1,59 @@
+import axios from "axios";
+import { useRouter } from "next/router";
 import React from "react";
 import { useRecoilValue } from "recoil";
 
 import { Button, Center, Text, VStack } from "@chakra-ui/react";
 
-import { RecoilOwner } from "@/store/Recoil";
+import { BASE_URL } from "@/data/BaseUrl";
+
+import { RecoilOwner, RecoilRoom } from "@/store/Recoil";
+
+import { HandleError } from "@/hooks/useError";
 
 import { CustomTitleText } from "../common/CustomTitleText";
 import { VSpacer } from "../common/Spacer";
+
 type Props = {
   theme: string;
   answer: string;
   isCorrect: boolean;
 };
+
 export const Result = ({ theme, answer, isCorrect }: Props) => {
+  const router = useRouter();
   const owner = useRecoilValue(RecoilOwner);
+  const room = useRecoilValue(RecoilRoom);
+
+  const Initialize = () => {
+    axios
+      .post(BASE_URL + "initialize", {
+        roomId: room.id,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          router.push("/wait");
+        }
+      })
+      .catch((err) => {
+        HandleError(router, err);
+      });
+  };
+
+  const FetchStep = () => {
+    axios
+      .get(BASE_URL + "step", {
+        params: { roomId: room.id },
+      })
+      .then((res) => {
+        if (res.data != 7) {
+          router.push("/wait");
+        }
+      })
+      .catch((err) => {
+        HandleError(router, err);
+      });
+  };
 
   return (
     <>
@@ -31,11 +71,11 @@ export const Result = ({ theme, answer, isCorrect }: Props) => {
           )}
           <VSpacer size={12} />
           {owner ? (
-            <Button colorScheme="linkedin" minW={64}>
+            <Button colorScheme="linkedin" minW={64} onClick={Initialize}>
               次へ
             </Button>
           ) : (
-            <Button colorScheme="linkedin" minW={64}>
+            <Button colorScheme="linkedin" minW={64} onClick={FetchStep}>
               更新
             </Button>
           )}
