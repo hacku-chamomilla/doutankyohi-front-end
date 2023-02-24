14afb22970a6398645c0ba06d96142672dac17b6
KEiJey <keijey1015@outlook.jp>
2023-02-24 21:08:18 +0900

    fix: 引数を変更

--- src/components/common/YouAre.tsx
+++ src/components/common/YouAre.tsx
@@ -1,47 +1,53 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 import { Button, Center, Image, Text, VStack } from "@chakra-ui/react";
 
 import { VSpacer } from "@/components/common/Spacer";
 
 type Props = {
-  title: string;
-  text: string;
-  areYou: boolean;
+  youAre: boolean;
+  setYouAre: Dispatch<SetStateAction<boolean>>;
 };
 
-export const YouAre = ({ title, text, areYou }: Props) => {
+export const YouAre = ({ youAre, setYouAre }: Props) => {
   return (
     <>
       <Center>
         <VStack>
-          {areYou ? (
+          {youAre ? (
             <>
               <Text fontSize={40} color={"red"}>
-                {title}
+                あなたは人狼です！
               </Text>
               <VSpacer size={8} />
               <Image src="https://bit.ly/3Zjpfj1" alt="wolf" />
               <VSpacer size={8} />
               <Text fontSize={28} color={"red"}>
-                {text}
+                正解しないように妨害をしよう！
               </Text>
             </>
           ) : (
             <>
               <Text fontSize={40} color={"blue"}>
-                {title}
+                あなたは村人です！
               </Text>
               <VSpacer size={8} />
               <Image src="https://bit.ly/3IylGyK" alt="vilager" />
               <VSpacer size={8} />
               <Text fontSize={28} color={"blue"}>
-                {text}
+                協力して正解に導こう！
               </Text>
             </>
           )}
           <VSpacer size={12} />
-          <Button colorScheme="pink" size="lg" minW={48}>
+          <Button
+            colorScheme="pink"
+            size="lg"
+            minW={48}
+            onClick={() => {
+              setYouAre(true);
+            }}
+          >
             Are you Ready?
           </Button>
         </VStack>
