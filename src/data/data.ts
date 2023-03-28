d81a8b241b53e7580426a67a9829f3213462c837
ltoppyl <toppyajibutu@gmail.com>
2023-03-28 16:38:18 +0900

    feat: BE のデプロイリンクを修正

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,10 +7,14 @@ export const avatarList = [
   "https://bit.ly/41j3GAD",
 ];
 
-export const BASE_URL = "http://localhost:1323/";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/"
+    : "https://doutankyohi-1-m6114301.deta.app/";
 
 export const INTERVAL = 10000;
 
-export const IS_AUTO_REQUEST = false;
+export const IS_AUTO_REQUEST =
+  process.env.NODE_ENV === "development" ? false : true;
 
 export const MAX_AUTO_HTTP_REQUEST = 20;
